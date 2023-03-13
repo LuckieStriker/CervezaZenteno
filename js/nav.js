d70e0dda187babaebc6ce937b97bebefa259a1f0
Miguel Morán <miguelrmor@gmail.com>
2023-03-13 16:05:17 -0600

    add: Se arregla nav.js, ya funciona, falta edición

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,19 @@
-$(function (){
-    $("#navigation").load("../html/nav.html");
-});
+
+let html="";
+function navbar(){
+    let links =[
+        ["Inicio","../index.html"],
+        ["Dónde encontrarnos", "../html/ubi.html"],
+        ["Envíos","../html/envios.html"],
+        ["Nosotros","../html/contacto.html"]
+    ];
+    
+    for (let i=0;i<links.length;i++){
+        let str=""+links[i];
+        str=str.split(',');
+        html+="<ul class='nav justify-content-end'><li class='nav-item'><a class='nav-link active' aria-current='page' href='"+str[1]+"'>"+str[0]+"</a></li></ul>";
+        document.getElementById('nav').innerHTML=html;
+    }   
+    
+}
+navbar();
